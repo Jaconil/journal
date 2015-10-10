96821c1fe8ea1cc7e4c30b580e7b93a705b38508
GUIHAL Maxime <sgp.jaconil@gmail.com>
2015-10-10 14:41:44 +0200

    Upgrade to React 0.14

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Router, Route, Redirect } from 'react-router';
 import createHashHistory from 'history/lib/createHashHistory';
 
@@ -8,12 +9,12 @@ import App from './components/App/app.jsx';
 import LoginBox from './components/LoginBox/loginBox.jsx';
 import WritePage from './components/WritePage/writePage.jsx';
 
-React.render((
-  <Router history={ createHashHistory({ queryKey: false }) }>
+ReactDOM.render((
+  <Router history={createHashHistory({ queryKey: false })}>
     <Route component={App}>
       <Route path="login" component={LoginBox}/>
       <Route path="write" component={WritePage}/>
       <Redirect from="/" to="/login" />
     </Route>
   </Router>
-), document.body);
+), document.querySelector('#app'));
