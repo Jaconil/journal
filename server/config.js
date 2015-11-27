16fc24acf3dbe854d7763497aaf4e33a4997473a
GUIHAL Maxime <sgp.jaconil@gmail.com>
2015-11-27 15:59:32 +0100

    Add server config file again

---
+++ server/config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var defaultConfig = {
+  port: 1337,
+
+  dbHost: null,
+  dbPort: null,
+  dbName: null,
+  dbUser: null,
+  dbPassword: null,
+
+  jwtSecret: 'secretKey'
+};
+
+// Env variables assignment
+module.exports = _.mapValues(defaultConfig, (defaultValue, key) => {
+  var snakeKey = _.snakeCase(key);
+  var value = process.env['npm_config_' + snakeKey] || process.env[snakeKey.toUpperCase()] || defaultValue;
+
+  if (value === null) {
+    throw new Error('Env variable ' + key.toUpperCase() + ' is required but undefined');
+  }
+
+  return value;
+});
