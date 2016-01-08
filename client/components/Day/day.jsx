38f5500e6997a22b59f6980c5a8db66fbbe276d1
GUIHAL Maxime <sgp.jaconil@gmail.com>
2016-01-08 17:09:30 +0100

    Confirmation 2nd draft

--- client/components/Day/day.jsx
+++ client/components/Day/day.jsx
@@ -24,10 +24,10 @@ class Day extends React.Component {
   constructor(props) {
     super(props);
     this.onCloseClick = this.onCloseClick.bind(this);
+    this.onBackClick = this.onBackClick.bind(this);
     this.onSubmitClick = this.onSubmitClick.bind(this);
     this.onSubmitEnter = this.onSubmitEnter.bind(this);
     this.handleFocus = this.handleFocus.bind(this);
-    this.onFocus = this.onFocus.bind(this);
 
     this.state = { confirmation: false };
   }
@@ -36,18 +36,23 @@ class Day extends React.Component {
     this.refs.container.classList.remove('focused');
   }
 
+  onBackClick() {
+    this.setState({ confirmation: false });
+  }
+
   onSubmitClick() {
-    //if (this.state.confirmation) {
-    //  this.onCloseClick();
-    //  this.props.dispatch(update(this.props.data.date, this.refs.content.value));
-    //  this.props.onSubmit();
-    //}
+    if (this.state.confirmation) {
+      this.onCloseClick();
+      this.props.dispatch(update(this.props.data.date, this.refs.content.value));
+      this.props.onSubmit();
+    }
 
     this.setState({ confirmation: !this.state.confirmation });
   }
 
   onSubmitEnter(event) {
     if (event.keyCode === 13 && !event.shiftKey) {
+      event.preventDefault();
       this.onSubmitClick();
     }
   }
@@ -56,10 +61,6 @@ class Day extends React.Component {
     if (!this.props.disabled && this.props.data.status === 'notWritten') {
       this.refs.content.focus();
     }
-  }
-
-  onFocus() {
-    this.refs.container.classList.add('focused');
   }
 
   componentDidMount() {
@@ -71,14 +72,18 @@ class Day extends React.Component {
   }
 
   render() {
-    var boxClasses = classNames('day', { disabled: this.props.disabled, confirmation: this.state.confirmation });
+    var boxClasses = classNames('day', {
+      disabled: this.props.disabled,
+      confirmation: this.state.confirmation,
+      focused: !this.props.disabled && this.props.data.status === 'notWritten'
+    });
     var statusClasses = classNames('status', _.kebabCase(this.props.data.status));
 
     var actions = null;
     var content = null;
     var confirmationOverlay = null;
 
-    if (!this.props.disabled && this.props.data.status === 'notWritten') {
+    if (!this.props.disabled && !this.state.confirmation && this.props.data.status === 'notWritten') {
       actions = <div className="actions">
         <button className="close" onClick={this.onCloseClick}><i className="fa fa-close"></i></button>
         <button className="submit" onClick={this.onSubmitClick}><i className="fa fa-check"></i></button>
@@ -86,15 +91,15 @@ class Day extends React.Component {
     }
 
     if (this.props.data.status === 'notWritten') {
-      content = <Textarea ref="content" disabled={this.props.disabled} onKeyUp={this.onSubmitEnter} onFocus={this.onFocus}></Textarea>;
+      content = <Textarea ref="content" disabled={this.props.disabled || this.state.confirmation} onKeyDown={this.onSubmitEnter}></Textarea>;
     } else {
       content = <div className="text">{this.props.data.content}</div>;
     }
 
     if (this.state.confirmation) {
       confirmationOverlay = <div className="confirmationOverlay">
-        <button className="back" onClick={this.onCloseClick}><i className="fa fa-back"></i></button>
-        <button className="submit" onClick={this.onSubmitClick}><i className="fa fa-check"></i></button>
+        <button onClick={this.onBackClick}><i className="fa fa-thumbs-down fa-2x"></i></button>
+        <button onClick={this.onSubmitClick}><i className="fa fa-thumbs-up fa-2x"></i></button>
       </div>;
     }
 
