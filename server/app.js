ea3485660a347f4a5aee950b88fd99924e519dfe
GUIHAL Maxime <sgp.jaconil@gmail.com>
2016-05-10 18:15:04 +0200

    Upgrade eslint part 1

--- server/app.js
+++ server/app.js
@@ -2,30 +2,30 @@
 
 global._ = require('lodash');
 
-var url = require('url');
-var path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser');
+const url = require('url');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var config = require('./config');
-var logger = require('./logger')();
-var db = require('mongoskin').db(decodeURI(url.format({
+const config = require('./config');
+const logger = require('./logger')();
+const db = require('mongoskin').db(decodeURI(url.format({
   protocol: 'mongodb',
   slashes: true,
   hostname: config.dbHost,
   port: config.dbPort,
   pathname: config.dbName,
-  auth: config.dbUser + ':' + config.dbPassword
+  auth: `${config.dbUser}:${config.dbPassword}`
 })));
 
-var app = express();
+const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.use('/api', require('./api')(express, db, logger, config));
 
-app.get(/^(?!\/api).*/, function(request, response) {
+app.get(/^(?!\/api).*/, (request, response) => {
   response.sendFile(path.resolve('public', 'index.html'));
 });
 
