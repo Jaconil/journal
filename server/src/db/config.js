013220f59674dc4efe2c18d78f857b9cf20c3879
GUIHAL Maxime <sgp.jaconil@gmail.com>
2017-11-21 21:51:44 +0100

    Fix db production environment

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,11 +1,15 @@
 /* eslint-disable no-process-env */
+
+const configuration = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  operatorsAliases: false
+};
+
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    operatorsAliases: false
-  }
+  development: configuration,
+  production: configuration
 };
